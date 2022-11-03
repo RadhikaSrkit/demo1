592256880549c208bc626e8582f2df920a5fff7b
radhika <radhikaankala@gmail.com>
2022-11-03 09:32:56 +0530

    file was created by radhika

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition
+ {
+   public static void main(String args[])
+   {
+     int a=10,c;
+     int b=20;
+    c=a+b;
+    System.out.println("Program on addition");
+    System.out.println("addition of two numbers="+c);
+  }
+}
